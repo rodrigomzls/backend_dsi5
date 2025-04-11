26ea8cf96a86b5a038f9eefcb184f3c804a84798
rodrigomzls <rrodrigomzls@gmail.com>
2025-04-10 21:07:07 -0500

    clientes - usuarios sin token

--- routes/cliente.routes.js
+++ routes/cliente.routes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const clienteController = require("../controllers/cliente.controller");
 const { verifyToken } = require("../middlewares/auth.middleware");
 
-router.post("/clientes",  verifyToken, clienteController.createCliente);
-router.get("/clientes",  verifyToken, clienteController.getClientes); 
-router.get("/clientes/:id", verifyToken, clienteController.getClienteById); 
-router.put("/clientes/:id", verifyToken, clienteController.updateCliente);
-router.delete("/clientes/:id", verifyToken, clienteController.deleteCliente);
+router.post("/clientes", clienteController.createCliente);
+router.get("/clientes",  clienteController.getClientes); 
+router.get("/clientes/:id", clienteController.getClienteById); 
+router.put("/clientes/:id", clienteController.updateCliente);
+router.delete("/clientes/:id", clienteController.deleteCliente);
 
 module.exports = router;
+    
--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -5,9 +5,9 @@ const { verifyToken } = require("../middlewares/auth.middleware");
 
 router.post("/usuarios", usuarioController.createUsuario);
 router.get("/usuarios", verifyToken, usuarioController.getUsuarios); 
-router.get("/usuarios/:id", verifyToken, usuarioController.getUsuarioById); 
-router.put("/usuarios/:id", verifyToken, usuarioController.updateUsuario);
-router.delete("/usuarios/:id", verifyToken, usuarioController.deleteUsuario);
+router.get("/usuarios/:id", usuarioController.getUsuarioById); 
+router.put("/usuarios/:id",  usuarioController.updateUsuario);
+router.delete("/usuarios/:id", usuarioController.deleteUsuario);
 
 
 module.exports = router;
